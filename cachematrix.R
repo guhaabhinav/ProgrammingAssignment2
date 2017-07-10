09e4cef5129da0d787ce90ffb32a2bcdbdc6d339
Abhinav <guhaabhinav@gmail.com>
2017-07-09 17:52:05 -0700

    coursera r programming assignment - matrix inversion, and/or cache store

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions to take in a "special" matrix, and return it's inverse
 
-## Write a short comment describing this function
+
+## First function remains more or less identical to the case of "special" vector
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## This function checks to see if the inverse matrix is cached 
+## (if so, returns it), or else computes it and returns the computed value
+## and stores the computed value in cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
+        
 }
